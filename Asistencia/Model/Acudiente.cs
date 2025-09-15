1c4e87fd91f59b584aedb8a51060fbdbf9a94410
Jhon Alejandro <torresgiraldojhon1224@outlook.es>
2025-09-14 19:10:07 -0500

    se agregan cambios en el modelo acudientes

---
+++ Asistencia/Model/Acudiente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asistencia.Model
+{
+    public class Acudiente
+    {
+        public int AcudienteId { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Documento { get; set; }
+
+        public string Tipo_Documento { get; set; }
+
+        public string telefono { get; set; }
+        public string Correo { get; set; }
+
+        public string parentesco { get; set; }
+
+        public int Alumno_id { get; set; }
+
+        //public virtual ICollection<Alumno> Alumnos { get; set; }
+    }
+}
--- Asistencia/Model/Profesores.cs
+++ Asistencia/Model/Profesores.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,33 @@ namespace Asistencia.Model
 {
     public class Profesores
     {
+        public int ProfesorId { get; set; }
+
+
+        [Required]
+        [StringLength(50)]
+        public string Nombres { get; set; }
+
+
+
+        [Required]
+        [StringLength(12)]
+        public string Documento { get; set; }
+
+
+
+        [Required]
+        [StringLength(50)]
+        public string Apellidos { get; set; }
+
+
+        [Required]
+        public string CorreoElectronico { get; set; }
+
+
+
+        [Required]
+        [StringLength(100)]
+        public string Direccion { get; set; }
     }
 }
